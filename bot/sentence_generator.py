53a8cc4be8740605a33fc67c75c8efeb57340882
anathexis <99439178+anathexis@users.noreply.github.com>
2022-06-18 21:40:12 +0200

    Update sentence_generator.py

--- bot/sentence_generator.py
+++ bot/sentence_generator.py
@@ -20,7 +20,6 @@ class TextFileSentenceGenerator(SentenceGeneratorBase):
 
     def draw_sentence(self, message: str) -> str:
         sentence_list = self.get_sentence_list_from_path()
-        print(message)
         if message.isnumeric() and int(message) < len(sentence_list):
             return sentence_list[int(message)]
         return choice(sentence_list)
